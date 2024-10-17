76b8c3ac64e1dfd458b51290e53edd46d58fe512
likith1003 <123715227+likith1003@users.noreply.github.com>
2024-10-17 18:36:44 +0530

    Add files via upload

--- todo/app/urls.py
+++ todo/app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('update<pk>', update, name='update'),
+    path('delete<pk>', delete, name='delete')
 ]
--- todo/app/views.py
+++ todo/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import *
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 
@@ -14,3 +15,21 @@ def home(request):
             todo = Todo(title=title, desc=desc)
             todo.save()
     return render(request, 'home.html', d)
+
+
+def update(request,pk):
+    TO = Todo.objects.get(pk=pk)
+    d = {'TO': TO}
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        desc = request.POST.get('desc')
+        TO.title=title
+        TO.desc=desc
+        TO.save()
+        return HttpResponseRedirect(reverse('home'))
+    return render(request, 'update.html', d)
+
+def delete(request, pk):
+    TO = Todo.objects.get(pk=pk)
+    TO.delete()
+    return HttpResponseRedirect(reverse('home'))
